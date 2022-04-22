2537033f49e52e4e2010a5b85978633869eedbba
Matej Badin <matej.badin@gmail.com>
2022-04-22 12:07:40 +0200

    Update phonons

--- EX06-phonon/Si/run.sh
+++
@@ -1,7 +0,0 @@
-
-PARA_PREFIX="mpirun -np 2"
-NOPA_PREFIX=""
-
-$PARA_PREFIX pw.x     < si.scf.in > si.scf.out
-$PARA_PREFIX ph.x     < si.ph.in  > si.ph.out
-$NOPA_PREFIX dynmat.x < si.dm.in  > si.dm.out
